945f2f3dcf5de6828709fa3925499402e8853d87
仕博 <1279257613@qq.com>
2023-12-12 23:48:17 +0800

    first commit

---
+++ utils/log4.js
@@ -0,0 +1,54 @@
+const log4js = require('log4js');
+
+const levels = {
+  trace: log4js.levels.TRACE,
+  debug: log4js.levels.DEBUG,
+  info: log4js.levels.INFO,
+  warn: log4js.levels.WARN,
+  error: log4js.levels.ERROR,
+  fatal: log4js.levels.FATAL,
+};
+
+log4js.configure({
+  appenders: {
+    default: { type: 'console' },
+    info: {
+      type: 'file',
+      filename: 'logs/info.log',
+    },
+    error: {
+      type: 'dateFile',
+      filename: 'logs/error',
+      pattern: 'yyyy-MM-dd-hh.log',
+      alwaysIncludePattern: true,
+    },
+  },
+  categories: {
+    default: { appenders: ['default'], level: 'debug' },
+    info: { appenders: ['info'], level: 'info' },
+    error: {
+      appenders: ['error'],
+      level: 'error',
+    },
+  },
+});
+
+function loggerType(ctx, type, content) {
+  ctx.level = levels[type];
+  ctx[type](content);
+}
+
+exports.debug = (content) => {
+  const logger = log4js.getLogger();
+  loggerType(logger, 'debug', content);
+};
+
+exports.info = (content) => {
+  const logger = log4js.getLogger('info');
+  loggerType(logger, 'info', content);
+};
+
+exports.error = (content) => {
+  const logger = log4js.getLogger('error');
+  loggerType(logger, 'error', content);
+};
---
+++ utils/useTool.js
@@ -0,0 +1,49 @@
+/**
+ * 通用工具函数
+ */
+const { error, info } = require('./log4.js');
+const CODE = {
+  SUCCESS: 200,
+  ERROR: 500,
+  PARAM_ERROR: 10001, // 参数错误
+  USER_ACCOUNT_ERROR: 20001, // 账号或密码错误
+  USER_LOGIN_ERROR: 30001, // 用户未登录
+  BUSINESS_ERROR: 40001, // 业务请求失败
+  AUTH_ERROR: 50001, // 认证失败或token错误
+};
+
+module.exports = {
+  /**
+   * 分页结构封装
+   * @param {number} pageNum
+   * @param {number} pageSize
+   */
+
+  pager(pageNum = 1, pageSize = 1) {
+    pageNum *= 1;
+    pageSize *= 1;
+    const skipIndex = (pageNum - 1) * pageSize;
+    return {
+      pageNum,
+      pageSize,
+      skipIndex,
+    };
+  },
+
+  success(data = '', msg = '', code = CODE.SUCCESS) {
+    info(msg);
+    return {
+      code,
+      data,
+      msg,
+    };
+  },
+  fail(msg = '', code = CODE.ERROR) {    
+    error(msg);
+    return {
+      code,
+      data: null,
+      msg,
+    };
+  },
+};
